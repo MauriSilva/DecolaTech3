fa5a2f2b82a11b480872bdd73c546675aab0e4ff
Mauricio <mauricioferrazsilva9@gmail.com>
2022-05-17 17:41:14 -0300

    Commit das pastas

---
+++ Javascript/Atividade pratica contador/script.js
@@ -0,0 +1,15 @@
+var currentNumberWrapper = document.getElementById("currentnumber")
+var currentnumber=0;
+
+function increment(){
+    currentnumber=currentnumber+1;
+    currentNumberWrapper.innerHTML=currentnumber;
+}
+
+function decrement(){
+    currentnumber=currentnumber-1;
+    currentNumberWrapper.innerHTML=currentnumber;
+}
+
+
+
